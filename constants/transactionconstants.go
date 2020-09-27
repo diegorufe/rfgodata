292f362234d05a2d67a75d5cc7cfdbc87ca29b6f
diegorufe <diegorufe@gmail.com>
2020-09-27 19:22:01 +0200

    Added transaction type

--- constants/transactionconstants.go
+++ constants/transactionconstants.go
@@ -10,3 +10,11 @@ const (
 	// ErrorTransactionNotFound : transaction not found error
 	ErrorTransactionNotFound ErrorTransactionType = 0xE000001
 )
+
+// TransactionType : Indicate type of transaction
+type TransactionType int
+
+const (
+	// TransactionGorm : transaction for gorm
+	TransactionGorm TransactionType = 0
+)
